501f35496e6a3fdef42d0de90a50f4f84d125075
waterv <help_5chan@outlook.com>
2021-10-22 17:49:05 +0800

    Add index.html

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<el-main>
+  <h2>迪士尼在逃小幸</h2>
+  <el-row>
+    <el-input v-model="input" placeholder="昵称" />
+  </el-row>
+  <el-row>
+    <el-button type="primary" @click="play">点我冲鸭</el-button>
+  </el-row>
+  
+  <el-divider />
+
+  <h2>排行榜</h2>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="rank" label="Rank" />
+    <el-table-column prop="nickname" label="ID" />
+    <el-table-column prop="score" label="Score" />
+  </el-table>
+</el-main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AV from 'leancloud-storage';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      input: '',
+      tableData: []
+    };
+  },
+  mounted () {
+    const leaderboard = AV.Leaderboard.createWithoutData('score');
+    leaderboard.getResults({
+      limit: 100,
+      selectUserKeys: ['nickname']
+    }).then(results => {
+      console.log(results);
+      this.$data.tableData = results.map(result => {
+        return {
+          rank: result.rank + 1,
+          nickname: result.user.attributes.nickname,
+          score: result.value
+        };
+      });
+      console.log(this.$data.tableData);
+    });
+  },
+  methods: {
+    play () {
+      location.href = 'game.html?id=' + this.$data.input;
+    },
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.el-row + .el-row {
+  padding-top: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
+// Vue
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Element +
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+
+// LeanCloud
+import AV from 'leancloud-storage';
+AV.init({
+    appId: 'EK27oracrqWzxLAcfhGIMOST-MdYXbMMI',
+    appKey: 'gWA7jWpfJzUM9dzLjc56bwFC'
+});
+
+createApp(App).use(ElementPlus).mount('#app')
